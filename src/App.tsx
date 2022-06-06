e6a0e7bab0e5f5e24984421eff82b52897305bc0
PCY71 <skypcy71@gmail.com>
2022-06-06 22:38:01 +0900

    html 완성

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import './App.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faYoutube } from '@fortawesome/free-brands-svg-icons';
-import { faThumbsUp, faThumbsDown, faShare, faPlus } from '@fortawesome/free-solid-svg-icons'
+import { faThumbsUp, faThumbsDown, faShare, faPlus, faEllipsis, faSearch, faE } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
 
   return (
     <>
-    {/* Header */}
+      {/* Header */}
       <header>
         <div className="logo">
           <FontAwesomeIcon icon={faYoutube} />
           <span className="title">Youtube</span>
         </div>
-        <div className="icons"></div>
+        <div className="icons">
+          <FontAwesomeIcon icon={faSearch} />
+          <FontAwesomeIcon icon={faEllipsis} />
+        </div>
       </header>
 
       {/* Video Player */}
@@ -42,35 +45,80 @@ function App() {
         <div className="actions">
           <li>
             <button>
-              <FontAwesomeIcon icon={faThumbsUp}/>
+              <FontAwesomeIcon icon={faThumbsUp} />
               <span>1K</span>
             </button>
           </li>
           <li>
             <button>
-              <FontAwesomeIcon icon={faThumbsDown}/>
+              <FontAwesomeIcon icon={faThumbsDown} />
               <span>0</span>
             </button>
           </li>
           <li>
             <button>
-              <FontAwesomeIcon icon={faShare}/>
+              <FontAwesomeIcon icon={faShare} />
               <span>share</span>
             </button>
           </li>
           <li>
             <button>
-              <FontAwesomeIcon icon={faPlus}/>
+              <FontAwesomeIcon icon={faPlus} />
               <span>Save</span>
             </button>
           </li>
         </div>
+
+        {/* Channel Description */}
         <div className="channel">
-          <div className="metadata"></div>
-          <div className="subscribe"></div>
+          <div className="metadata">
+            <div className="info">
+              <span className="name">
+                CY's Study Room
+              </span>
+              <span className="subscribers">
+                1M subscribers
+              </span>
+            </div>
+          </div>
+          <button className="subscribe">subscribe</button>
         </div>
       </section>
-      <section className="UpNext"></section>
+      <section className="UpNext">
+        <span className="title">Up next</span>
+        <ul>
+          <li className="item">
+            <img src="" alt="img1" />
+            <div className="info">
+              <span className="title">
+                HTML 기초, React 할때 꼭 필요한 팁! 쉽다고 무시하면 안돼요 | 프론트엔드 개발자 입문편: HTML, CSS, JavaScript
+              </span>
+              <span className="channel">CY's Study Room</span>
+              <span className="views">1M views</span>
+            </div>
+            <button className="moreBtn"><FontAwesomeIcon icon={faEllipsis}/></button>
+          </li>
+          <li className="item">
+            <img src="" alt="img2" />
+            <span className="title">
+              HTML 태그 완성, 웹 포트폴리오 만들기 전에 꼭 보세요 | 프론트엔드 개발자 입문편: HTML, CSS, JavaScript
+            </span>
+            <span className="channel">CY's Study Room</span>
+            <span className="views">82K views</span>
+            <button className="moreBtn"><FontAwesomeIcon icon={faEllipsis}/></button>
+          </li>
+          <li className="item">
+            <img src="" alt="img3" />
+            <span className="title">
+              HTML 태그 완성, 웹 포트폴리오 만들기 전에 꼭 보세요 | 프론트엔드 개발자 입문편: HTML, CSS, JavaScript
+            </span>
+            <span className="channel">CY's Study Room</span>
+            <span className="views">82K views</span>
+            <button className="moreBtn"><FontAwesomeIcon icon={faEllipsis}/></button>
+
+          </li>
+        </ul>
+      </section>
     </>
   )
 }
